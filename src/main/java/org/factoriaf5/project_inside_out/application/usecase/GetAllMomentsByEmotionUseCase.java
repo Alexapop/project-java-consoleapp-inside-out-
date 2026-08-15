7373ae4498f625efac6ffe56cc6f76f8214a8fdf
Ioana <ioana3popescu@gmail.com>
2026-08-15 19:17:31 +0200

    feat: add use case for get all moments by emotion

---
+++ src/main/java/org/factoriaf5/project_inside_out/application/usecase/GetAllMomentsByEmotionUseCase.java
@@ -0,0 +1,20 @@
+package org.factoriaf5.project_inside_out.application.usecase;
+
+import java.util.List;
+
+import org.factoriaf5.project_inside_out.domain.repository.MomentRepository;
+import org.factoriaf5.project_inside_out.domain.entities.Emotion;
+import org.factoriaf5.project_inside_out.domain.entities.Moment;
+
+public class GetAllMomentsByEmotionUseCase {
+    private MomentRepository momentRepository;
+
+public GetAllMomentsByEmotionUseCase (MomentRepository momentRepository){
+    this.momentRepository=momentRepository;
+}
+
+public List<Moment> execute ( Emotion emotion){
+    return momentRepository.filterByEmotion(emotion);
+
+}
+}
